2c8d3af1678ac509ee59b1e6d845f9e1cea116cf
BuildTools <arthurpisakhov1@gmail.com>
2019-10-21 20:03:49 -0400

    POST

--- server.js
+++ server.js
@@ -14,4 +14,17 @@ server.get('/api/posts', (req, res) => {
     .catch(() => res.status(500).json({ error: "The users information could not be retrieved." }))
 })
 
+server.post('/api/posts', (req, res) => {
+  const { title, content } =  req.body;
+
+  if (!title || !content) {
+    res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+  } else {
+    postList
+      .find()
+      .then(post => res.status(201).json(post))
+      .catch(() => res.status(500).json({ error: "There was an error while saving the post to the database" }))
+  }
+})
+
 server.listen(5000, () => console.log('Server running on http://localhost:5000'))
